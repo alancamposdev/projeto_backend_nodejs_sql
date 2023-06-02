592c8d925e7ae69efa31d224ebfd9655835b53b5
AlanCampos <alancamposdeveloper@gmail.com>
2023-06-02 14:46:44 -0400

    criando componentes da home

--- frontend/portal-cpt/src/App.jsx
+++ frontend/portal-cpt/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter, Routes,  Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Cadastro from './components/CadastroAluno';
+import Login from './components/Login';
+import './assets/css/global.css';
 
 
-function App() {
-  //const [count, setCount] = useState(0)
 
-  return (
+
+
+const App = () => { 
+
+
+  return (   
+
     <div>
-      <h1>Portal CPT</h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/cadastro" element={<Cadastro/>} />
+            <Route path="/login" element={<Login/>} />
+          </Routes>
+        </BrowserRouter>      
     </div>
   )
 }
